c7d9824caea9b7cc6cdada08a91a36560ed19458
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2024-10-08 19:38:26 +0900

    fix

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+  maxHttpBufferSize: 1e8
+});
 
 const PORT = 3000;
 let connectedUsers = 0;
